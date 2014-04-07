697879b609ca96a3420445b004b1e38060390471
jaumet <jaume@nualart.cat>
2014-04-07 23:10:42 +1000

    Multilanguage implemented. Pending of adding all the necessary langstrings

---
+++ data/mylang.js
@@ -0,0 +1,11 @@
+var lang_ca = 
+{
+  "mytitle":"L'ULL!!!!!",
+  "my_youarereading":"Est&agrave;s llegint:"
+};
+
+var lang_es = 
+{
+  "mytitle":"EL OJO!!!!!",
+  "my_youarereading":"Est&aacute;s leyendo:"
+};
