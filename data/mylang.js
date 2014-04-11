853142b02e6b5b130715ba3aabcaa6cc9df44cf6
jaumet <jaume@nualart.cat>
2014-04-11 14:41:12 +1000

    Added support for other pages

--- data/mylang.js
+++ data/mylang.js
@@ -1,11 +1,25 @@
 var lang_ca = 
 {
   "mytitle":"",
-  "my_youarereading":"Est&agrave;s llegint:"
+  "my_youarereading":"Est&agrave;s llegint:",
+  "about":"Sobre",
+  "feedback":"Comenta",
+  "credits":"Cr&egrave;dits",
+  "page-default":"page per defecte",
+  "page-about":"page Sobre",  
+  "page-feedback":"page Comenta",
+  "page-credits":"page Cr&egrave;dits",
 };
 
 var lang_es = 
 {
   "mytitle":"",
-  "my_youarereading":"Est&aacute;s leyendo:"
+  "my_youarereading":"Est&aacute;s leyendo:",
+  "about":"Sobre",
+  "feedback":"Comenta",
+  "credits":"Cr&eacute;ditos",
+  "page-default":"page por defecto",
+  "page-about":"page Sobre",  
+  "page-feedback":"page Comenta",
+  "page-credits":"page Cr&eacute;ditos",
 };
