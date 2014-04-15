37adf629992a1325c1e034113e5d97e6e0706200
jaumet <jaume@nualart.cat>
2014-04-15 16:35:36 +1000

    Added one click all-text view in a pop-up DIV

--- data/mylang.js
+++ data/mylang.js
@@ -1,9 +1,14 @@
 var lang_ca = 
 {
-  "mytitle":"",
+  "mytitle":"<b>L'Ull escolta - Eugeni Bonet</b>: pantalles, projeccions, escrits",
   "my_youarereading":"Est&agrave;s llegint:",
   "allText":"Tot el text",
   "about":"Sobre",
+  "mytexts":"texts",
+  "mycatalog":"Cat&agrave;leg",
+  "mycritic":"Cr&iacute;tica",
+  "myopinion":"Opini&oacute;",
+  "myothers":"Altres",
   "feedback":"Comenta",
   "credits":"Cr&egrave;dits",
   "page-default":"page per defecte",
@@ -14,10 +19,15 @@ var lang_ca =
 
 var lang_es = 
 {
-  "mytitle":"",
+  "mytitle":"<b>El ojo escucha - Eugeni Bonet</b>: pantallas, proyecciones, escritos",
   "my_youarereading":"Est&aacute;s leyendo:",
   "allText":"Todo el texto",
   "about":"Sobre",
+  "mytexts":"textos",
+  "mycatalog":"Cat&aacute;logo",
+  "mycritic":"Cr&iacute;tica",
+  "myopinion":"Opini&oacute;n",
+  "myothers":"Otros",
   "feedback":"Comenta",
   "credits":"Cr&eacute;ditos",
   "page-default":"page por defecto",
