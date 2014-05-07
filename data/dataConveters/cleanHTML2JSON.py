5534e031fe1dcc42b34a76376fe71a2f3464292d
jaumet <jaume@nualart.cat>
2014-05-07 18:54:22 +1000

    DATA specific: added cleanHTML2JSON

--- data/dataConveters/cleanHTML2JSON.py
+++ data/dataConveters/cleanHTML2JSON.py
@@ -1,31 +1,49 @@
-import os, re
+import os, fnmatch, re
 
-with open("my.html") as f:
-    data = f.read()
-    '''Build a list splitting data into "<p>*.</p>" pieces'''
-    paragraphs = re.compile("(?<!^)\s+(?=\<p\>)(?!.\s)").split(data)
+path = '/var/www/crossreads/data/dataConveters/DOCs-cp/HTMLs'
+filePattern= '*.html'
+print "INITIAL"
 
-'''Join paragraphs with len < 1100 characters (including spaces)'''
-paragraphs1 = []
-for i, p in enumerate(paragraphs):
-  p = re.sub("\n", "", p)
-  if i==0:
-    paragraphs1.append(p)
-  else:
-    if len(paragraphs[i-1]) + len(paragraphs[i])<1100:
-      paragraphs1[-1] += p
-    else:
-      paragraphs1.append(p)
+def replace(directory, filePattern):
+  path1 = directory+'/test'
+  path2 = directory+'/texts'
+  for path, dirs, files in os.walk(os.path.abspath(path1)):
+      for filename in fnmatch.filter(files, filePattern):
+        print "#########"+filename
+        filepath = os.path.join(path, filename)
+        print "--> "+filepath
+        with open(filepath) as f:
+          data = f.read()
+          '''Build a list splitting data into "<p>*.</p>" pieces'''
+          paragraphs = re.compile("(?<!^)\s+(?=\<p\>)(?!.\s)").split(data)
 
-'''JSON output'''    
-myjson = "[\n"
-for i, p1 in enumerate(paragraphs1):
-  myjson +=  "\t{\n\t\t\"pid\" : "+str(i)+",\n\t\t\"p\" : \""+p1+"\"\n\t},\n"
+        '''Join paragraphs with len < 1100 characters (including spaces)'''
+        paragraphs1 = []
+        for i, p in enumerate(paragraphs):
+          p = re.sub("\n", "", p)
+          if i==0:
+            paragraphs1.append(p)
+          else:
+            if len(paragraphs[i-1]) + len(paragraphs[i])<1100:
+              paragraphs1[-1] += p
+            else:
+              paragraphs1.append(p)
 
-myjson += "\n]"
-print myjson
+        '''JSON output'''    
+        myjson = "[\n"
+        for i, p1 in enumerate(paragraphs1):
+          myjson +=  "\t{\n\t\t\"pid\" : "+str(i)+",\n\t\t\"p\" : \""+p1+"\"\n\t},\n"
+        myjson += "\n]"
 
-#print "############"
-#print "from: "+str(len(paragraphs))
-#print "to: "+str(len(paragraphs1))
-#print "############"
+        with open(path2+"/"+filename, "w+") as f:
+          print ">>>>>>>> "+path2+"/"+filename
+          f.write(myjson)
+  #print "####  DEBUG  ########"
+  #print "from: "+str(len(paragraphs))
+  #print "to: "+str(len(paragraphs1))
+  #print "############"
+
+
+replace(path, filePattern)
+
+
