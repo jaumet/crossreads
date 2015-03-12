215030d6eaf6510dbefd50ff1b4d6953d0cb5915
jaumet <jaume@nualart.cat>
2015-03-13 09:49:09 +1100

    tiny

--- 3-crossreads/interface/js/main.js
+++ 3-crossreads/interface/js/main.js
@@ -1,1 +1,18 @@
 
+var  svg = d3.select("body")
+    .append("circle")
+         .attr("class", "logo")
+         .attr("cx", 225)
+         .attr("cy", 225)
+         .attr("r", 20)
+         .style("fill", "transparent")       
+         .style("stroke", "black")     
+         .style("stroke-width", 0.25)
+         .on("mouseover", function(){ 
+               d3.select(this)
+                   .style("fill", "url('http://localhost/crossreads/3-crossreads/interface/img/th/a4746001h.jpg')");
+         })
+          .on("mouseout", function(){ 
+               d3.select(this)
+                   .style("fill", "transparent");
+         });
