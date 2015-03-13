6133e5f69185865e6e01e909b529e56c1fb2f555
jaumet <jaume@nualart.cat>
2015-03-14 00:44:59 +1100

    Reading dummydata from angularjs

--- 3-crossreads/interface/js/app.js
+++ 3-crossreads/interface/js/app.js
@@ -1,14 +1,13 @@
 
 (function(){
-	var app = angular.module('diaries', []);
+	var app = angular.module('library', []);
 
 	app.controller('DiariesController', function(){
-		this.diary = mydiary;
-
-	
+		this.diary = DATA;
 	});
 
 	var mydiary = 	
+	[
 	{
 		"id":2,
 		"diary_id":"2",
@@ -19,7 +18,22 @@
 		"topics":"peace",
 		"transcript":"transcript",
 		"cover":"a3368001h.jpg"
-	};
+	},
+	{
+		"id":2,
+		"diary_id":"2",
+		"page_no":"2",
+		"title":"title",
+		"date":"1914/1/18",
+		"location":"Gallipolly",
+		"topics":"peace",
+		"transcript":"transcript",
+		"cover":"a3368001h.jpg"
+	}
+	];
+
+
+
 
 
 }());
