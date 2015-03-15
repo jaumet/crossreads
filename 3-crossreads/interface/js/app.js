b6bc7e74c4a490fb7934a5d13caac10ee70061b5
jaumet <jaume@nualart.cat>
2015-03-15 22:32:40 +1100

    WOrking on the interface 3-corssreads. Starting with angularjs schema + dummydata generator + svg bezier tests +...

--- 3-crossreads/interface/js/app.js
+++ 3-crossreads/interface/js/app.js
@@ -4,37 +4,7 @@
 
 	app.controller('DiariesController', function(){
 		this.diary = DATA;
-	});
-
-/*
-	var mydiary = 	
-	[
-	{
-		"id":2,
-		"diary_id":"2",
-		"page_no":"2",
-		"title":"title",
-		"date":"1914/1/18",
-		"location":"Gallipolly",
-		"topics":"peace",
-		"transcript":"transcript",
-		"cover":"a3368001h.jpg"
-	},
-	{
-		"id":2,
-		"diary_id":"2",
-		"page_no":"2",
-		"title":"title",
-		"date":"1914/1/18",
-		"location":"Gallipolly",
-		"topics":"peace",
-		"transcript":"transcript",
-		"cover":"a3368001h.jpg"
-	}
-	];
-*/
-
-
-
-
+  });
+  
+	
 }());
