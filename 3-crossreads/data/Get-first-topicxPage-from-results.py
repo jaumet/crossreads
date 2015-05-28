efcd6dc6cf8b9e6f136e08d0c7d34b404cf9dc2e
jaumet <jaume@nualart.cat>
2015-05-28 18:51:08 +1000

    data: building mallet and topics

---
+++ 3-crossreads/data/Get-first-topicxPage-from-results.py
@@ -0,0 +1,63 @@
+import csv
+
+topics = ['Analysis & family', 'Cities & landscapes', 'Travelling', 'War', 'Militar life']
+
+f = open('results/20topics/my_compostion.csv')
+my20 = csv.reader(f)
+t0 = 0;t1=0;t2=0;t3=0;t4=0;co=0
+out='['
+# Transform 20 topics to 5
+c = [[],[],[],[],[]]
+current = ''
+for row20x in my20:
+  co+=1;row5 = [];tmp = []
+  out += '\n['
+  # in mycsv topics go from column 2 to 22 
+  # out put is 5 columns: (6, 9, 10, 20), (2, 11, 13, 17, 18), (12, 14, 16), (7, 19, 21), (3, 4, 5, 8) NOT included: 15 +float(row20x[15]))
+  row5.append(row20x[1].replace('file:/home/jaume/apps/Mallet/Diaries-superclean-text/',''))
+  tmp.append(float(row20x[6])+float(row20x[9])+float(row20x[10])+float(row20x[20]))
+  tmp.append(float(row20x[2])+float(row20x[11])+float(row20x[13])+float(row20x[17])+float(row20x[18]))
+  tmp.append(float(row20x[12])+float(row20x[14])+float(row20x[16]))
+  tmp.append(float(row20x[7])+float(row20x[19])+float(row20x[21]))
+  tmp.append(float(row20x[3])+float(row20x[4])+float(row20x[5])+float(row20x[8]))
+  m = max(tmp)
+  mymax = [i for i, j in enumerate(tmp) if j == m]
+  #row5.extend(mymax)
+
+'''  
+  diary = row20x[1].replace('file:/home/jaume/apps/Mallet/Diaries-superclean-text/','').split('/')[0]
+  if current == str(diary):
+    out += str(mymax)+', '
+  else:
+    current = diary
+    out += '], \n['+str(mymax)+', '
+  print co, out
+
+
+
+#    print len(row20x)
+#    print tmp
+  #print row5
+  #print
+#  print mymax[0],
+#  print tmp[mymax[0]],
+
+  # result:
+'''
+  
+  if int(mymax[0]) == 0:
+    t0 += 1
+  if int(mymax[0]) == 1:
+    t1 += 1
+  if int(mymax[0]) == 2:
+    t2 += 1
+  if int(mymax[0]) == 3:
+    t3 += 1
+  if int(mymax[0]) == 4:
+    t4 += 1
+
+
+print
+print t0, t1, t2, t3 , t4
+# write it out as pagesTopic.json
+
---
+++ 3-crossreads/data/dataDiaries--edit-reindex.py
@@ -0,0 +1,36 @@
+import json
+import simplejson
+from pprint import pprint
+import re, random, os
+
+def gen_simple_topics():
+  with open('dataDiaries.json') as data_file:    
+	  diaries = json.load(data_file)
+	  print diaries[126]["don"]
+  new = ''; 
+  out = "var DATA = [\n";
+  c = 1
+  for diary in diaries:
+    #if int(diary["id"])>17:
+    diary["id"] = c
+    c += 1
+    #print "id-> "+str(diary["id"]),
+    out += '{"id": '+str(diary["id"])+', \n\t"diary_id": '+str(diary["diary_id"])+', \n\t"don": '+diary["don"]+',  \n\t"title": "'
+    out += diary["title"]+'", \n\t"author": "'+diary["author"]+'", \n\t"topics": "'+diary["topics"]
+    out += '", \n\t"cover": "'+diary["cover"]+'",'
+    out += ' \n\t"start_date": [{"start_year": "'+diary["start_date"][0]["start_year"]+'", "start_month": "'+diary["start_date"][0]["start_month"]+'", "start_day": "'+diary["start_date"][0]["start_day"]+'"}],'
+    out += ' \n\t"end_date": [{"end_year": "'+diary["end_date"][0]["end_year"]+'", "end_month": "'+diary["end_date"][0]["end_month"]+'", "end_day": "'+diary["end_date"][0]["end_day"]+'"}],'
+    out += '\n\t"kind": "'+diary["kind"]+'\n\t"location": "'+diary["location"]+'", \n\t"page_no": "'+str(diary["page_no"])+'"\n},\n';
+        
+    #.encode("ascii")
+    #print
+    #new += out
+  new = out[:-2]+"];"
+    
+  f = open('XdataDiaries.js','w+')
+  f.write(str(new))
+  #simplejson.dump(, f)
+  f.close()
+
+
+gen_simple_topics()
