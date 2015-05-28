e209eef7c01d07201a5cb455b1523bf710ae631b
jaumet <jaume@nualart.cat>
2015-05-28 23:41:45 +1000

    3: dataTopics.js done for 128 diaries and mmore than 12000 pages

--- 3-crossreads/data/Get-first-topicxPage-from-results.py
+++ 3-crossreads/data/Get-first-topicxPage-from-results.py
@@ -5,13 +5,13 @@ topics = ['Analysis & family', 'Cities & landscapes', 'Travelling', 'War', 'Mili
 f = open('results/20topics/my_compostion.csv')
 my20 = csv.reader(f)
 t0 = 0;t1=0;t2=0;t3=0;t4=0;co=0
-out='['
+out='var topicMatrix = [\n['
 # Transform 20 topics to 5
 c = [[],[],[],[],[]]
 current = ''
 for row20x in my20:
   co+=1;row5 = [];tmp = []
-  out += '\n['
+  #out += '['
   # in mycsv topics go from column 2 to 22 
   # out put is 5 columns: (6, 9, 10, 20), (2, 11, 13, 17, 18), (12, 14, 16), (7, 19, 21), (3, 4, 5, 8) NOT included: 15 +float(row20x[15]))
   row5.append(row20x[1].replace('file:/home/jaume/apps/Mallet/Diaries-superclean-text/',''))
@@ -23,41 +23,43 @@ for row20x in my20:
   m = max(tmp)
   mymax = [i for i, j in enumerate(tmp) if j == m]
   #row5.extend(mymax)
+  mymax[0] += 1
+  if str(mymax[0]) != '':
+    diary = row20x[1].replace('file:/home/jaume/apps/Mallet/Diaries-superclean-text/','').split('/')[0]
+    if current == str(diary):
+      out += str(mymax[0])+','
+    else:
+      out = out[:-1]      
+      current = str(diary)
+      out += '], \n[0,'+str(mymax[0])+', ' ##+current
+  
 
-'''  
-  diary = row20x[1].replace('file:/home/jaume/apps/Mallet/Diaries-superclean-text/','').split('/')[0]
-  if current == str(diary):
-    out += str(mymax)+', '
-  else:
-    current = diary
-    out += '], \n['+str(mymax)+', '
-  print co, out
 
-
-
-#    print len(row20x)
-#    print tmp
+  #    print len(row20x)
+  #     print tmp
   #print row5
   #print
-#  print mymax[0],
-#  print tmp[mymax[0]],
+  #  print mymax[0],
+  #  print tmp[mymax[0]],
 
   # result:
-'''
   
-  if int(mymax[0]) == 0:
-    t0 += 1
+  
   if int(mymax[0]) == 1:
-    t1 += 1
+    t0 += 1
   if int(mymax[0]) == 2:
-    t2 += 1
+    t1 += 1
   if int(mymax[0]) == 3:
-    t3 += 1
+    t2 += 1
   if int(mymax[0]) == 4:
+    t3 += 1
+  if int(mymax[0]) == 5:
     t4 += 1
+out = out[:-1]
+out += ']\n];'
 
+print out
 
-print
-print t0, t1, t2, t3 , t4
+#print t0, t1, t2, t3 , t4
 # write it out as pagesTopic.json
 
