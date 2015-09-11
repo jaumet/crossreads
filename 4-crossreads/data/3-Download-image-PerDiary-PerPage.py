1bce117465b44111e61ed6e94f06dd95c05edd37
jaumet <jaume@nualart.cat>
2015-09-11 12:07:10 +1000

    tiny

--- 4-crossreads/data/3-Download-image-PerDiary-PerPage.py
+++ 4-crossreads/data/3-Download-image-PerDiary-PerPage.py
@@ -4,57 +4,48 @@ import json
 from os import listdir
 from os.path import isfile, join
 
-# Browse dirctory. And for each file do:
 mypath = "2-PagesList-forEachDiary"
 diaries = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 
 for d in diaries:
-    #print "xxxx"
     raw = '2-PagesList-forEachDiary/'+d
     # Open a Diary with Page List 
     print raw
-    if not (os.path.exists('3-Transcriptions/'+str(d[:-5])) or str(d[:-5])== "115225"):
-        with open(raw) as data_file:
-            diary = json.load(data_file)
-            #print diary[0]['transcript_pages']
-            myurl = ''
-            for page in diary[0]['transcript_pages']:
-                if type(page) is dict:
-                    myurl =  page['uri']
-                    print page['uri']
-                    #print "Is a dict"
-                elif type(page) is unicode:
-                    print "is a "+str(type(page))
-                    #import re
-                    myurl = diary[0]['transcript_pages'][page]['uri']
-                    print diary[0]['transcript_pages'][page]['uri']
-                    #m = re.search('(http\:\/\/.+)\"\,\"id', page)
-                    #print m
-                else:
-                    print "CHECK THISSSSSSSSSS"
-                
-                #print page["uri"] #["transcript_pages"]["id"] #['id']
-                response = urllib2.urlopen(myurl)
-                myjson = json.load(response)
+    #if not os.path.exists('3-Transcriptions/'+str(d[:-5])):
+    with open(raw) as data_file:
+        diary = json.load(data_file)
+        myurl = ''
+        for page in diary[0]['transcript_pages']:
+            if type(page) is dict:
+                myurl =  page['uri']
+                print page['uri']
+            elif type(page) is unicode:
+                print "is a "+str(type(page))
+                myurl = diary[0]['transcript_pages'][page]['uri']
+                print diary[0]['transcript_pages'][page]['uri']
+            else:
+                print "CHECK THISSSSSSSSSS"            
+            response = urllib2.urlopen(myurl)
+            myjson = json.load(response)
+            newdir = '3-Transcriptions/'+str(d[:-5])
+            if not os.path.exists(newdir+'/'+myjson['nid']+'.jpg'):
                 try:
-                    transcription = myjson['transcription']['value']
+                    myurl = myjson['transcript_image']['file']['uri']
+                    print "---> "+str(myurl)
+                    #if not os.path.exists(newdir): os.makedirs(newdir)
+                    response2 = urllib2.urlopen(myurl)
+                    myjson2 = json.load(response2)
+                    imgurl = myjson2['url']
                 except:
-                    transcription = "ERROR"
-                print transcription
-                if transcription is not ("<p>[Blank]</p>" and "ERROR"):
-                    #check if directory exists and create it
-                    newdir = '3-Transcriptions/'+str(d[:-5])
-                    if not os.path.exists(newdir): os.makedirs(newdir)
-                    if not os.path.exists(newdir+'/'+myjson['nid']+'.txt'):
-                        file = open(newdir+'/'+myjson['nid']+'.txt', "w")
-                        transcription = transcription.encode('utf-8').strip()
-                        file.write(transcription)
-                        file.close()
-                        print "Created --> "+newdir+'/'+myjson['nid']+'.txt'
+                    imgurl = "http://teatre.com/parts-of-the-leg.jpg"
+                print imgurl
+                #if not os.path.exists(newdir+'/'+myjson['nid']+'.jpg'):
+                img = urllib2.urlopen(imgurl)
+                file = open(newdir+'/'+myjson['nid']+'.jpg', "wb")
+                file.write(img.read())
+                file.close()
+                print "Created --> "+newdir+'/'+myjson['nid']+'.jpg'
                 print "------------"
-                ## Idemfor the image-file:
-                ##image = myjson['transcript_image']['file']['uri']
-            
 print
 print "This is it"
 
