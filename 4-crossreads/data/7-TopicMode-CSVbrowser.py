0d827955226e033803af61b90fa0c9ea26f85353
jaumet <jaume@nualart.cat>
2015-10-02 01:03:53 +1000

    4) Browser pointing to 100 topics csv file

--- 4-crossreads/data/7-TopicMode-CSVbrowser.py
+++ 4-crossreads/data/7-TopicMode-CSVbrowser.py
@@ -2,29 +2,40 @@ import csv
 import sys
 import operator
 
-# Get arguments
-arrArg = []
-for arg in sys.argv:
-     arrArg.append(arg)
-     print arg
-     
 # Open csv file and sorted it
 def sortFilterPrint(id, num, order):
-    mycsv = '6-Mallet-results/topics50/crossreads_compostion.txt'
+    mycsv = '6-Mallet-results/crossreads_compostion.txt'
     #mycsv = '6-Mallet-results/TM-test.csv'
-    mycsv = csv.reader(open(mycsv),delimiter='\t')
-    sortedlist = sorted(mycsv, key=operator.itemgetter(int(id)+2), reverse=int(order)) 
+    mycsv = csv.reader(open(mycsv),delimiter=',')
+    print "len of mycsv: "
+    setOrder = {"desc":1,"asc":0}
+    sortedlist = sorted(mycsv, key=operator.itemgetter(int(id)+2), reverse=setOrder[order]) 
 
     # Get every page path/filename
     for item in sortedlist[:int(num)]:
         #print item
-        print item[1]
-        new = item[1].replace('file:/home/jnualart/apps/Mallet/crossreads4', '3')
+        new = item[1].replace('file:/var/www/crossreads/4-crossreads/data/3-TranscriptionsCLEAN/', '3-TranscriptionsTXT/')
+        print new+"\t"+item[0]+"\t"+item[int(id)+2]
         page = open(new, 'r')
         print page.read()
         print "......................................"
 
-sortFilterPrint(arrArg[1], arrArg[2], arrArg[3  ])
+# Get arguments
+arrArg = []
+if len(sys.argv) <= 1 or len(sys.argv) > 4:
+    print
+    print "\tUsage: python 7-TopicMode-CSVbrowser.py [TM id] [Num pages] [desc/asc]"
+    print "\tThis script needs three parameters:"
+    print "\t* [TM id]: id of the topic model acording to csv files in './6-Mallet-results'"
+    print "\t* [Num pages]: Number of pages you want to see"
+    print "\t* [desc/asc]: sort descending or ascending"
+    print
+else:
+    for arg in sys.argv:
+         arrArg.append(arg)
+         print arg,
+
+    sortFilterPrint(arrArg[1], arrArg[2], arrArg[3])
 
 
 
