fdf6d099cf77b8340de3e335d48b1745f8e317e1
jaumet <jaume@nualart.cat>
2015-10-06 18:11:56 +1100

    4) R analysis

--- 4-crossreads/data/7-CSVbrowser-by-TopicModel-id.py
+++ 4-crossreads/data/7-CSVbrowser-by-TopicModel-id.py
@@ -7,18 +7,20 @@ def sortFilterPrint(id, num, order):
     file = '6-Mallet-results/crossreads4_compostion.txt'
     #file = '6-Mallet-results/test.csv'
     mycsv = csv.reader(open(file),delimiter=',')
-    print "###"
-    print type(mycsv)
     setOrder = {"desc":1,"asc":0}
     sortedlist = sorted(mycsv, key=lambda row: float(row[int(id)+2]), reverse=setOrder[order]) 
+    print
 
     # Get every page path/filename
+    c = 1
     for item in sortedlist[:int(num)]:
+        print "#"+str(c)
         new = item[1].replace('file:/home/jaume/apps/Mallet/TranscriptionsCLEAN/', '3-Transcriptions/')
-        print new+"\t"+item[0]+"\t"+item[int(id)+2]
+        print " -> "+new+"\t"+item[0]+"\t"+item[int(id)+2]
         page = open(new, 'r')
         print page.read()
-        print "......................................"
+        print "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+        c += 1
 
 # Get arguments
 arrArg = []
@@ -28,13 +30,13 @@ if len(sys.argv) <= 1 or len(sys.argv) > 4:
     print "\tThis script needs three parameters:"
     print "\t* [TM id]: id of the topic model acording to csv files in './6-Mallet-results'"
     print "\t* [Num pages]: Number of pages you want to see"
-    print "\t* [desc/asc]: sort descending or ascending"
+    print "\t* [desc/asc]: sort descending or ascending. [Defaul] is desc."
     print
 else:
     for arg in sys.argv:
          arrArg.append(arg)
-         print arg,
-
+    if len(arrArg) == 3:
+        arrArg.append("desc") # default value
     sortFilterPrint(arrArg[1], arrArg[2], arrArg[3])
 
 
