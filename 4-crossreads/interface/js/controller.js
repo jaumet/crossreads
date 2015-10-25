9a4cde43476ff2a3196c96401071c40b64a25b4f
jaumet <jaume@nualart.cat>
2015-10-25 15:53:54 +1100

    4) interface following ToDo.txt

--- 4-crossreads/interface/js/controller.js
+++ 4-crossreads/interface/js/controller.js
@@ -13,7 +13,7 @@ function listControl($scope,$http){
 		label = "unknown";
 		$scope.topics.forEach(function(el){
 			if (el.mallet_ids_inluded.indexOf(mallet_id) != -1) {
-				label = "Top topic in this diary: "+el.label;
+				label = el.label;
 			}
 		});
 		return label;
@@ -21,17 +21,56 @@ function listControl($scope,$http){
 
 	//$scope.orderFields = ["start_date[0].start_year","author"]
 
-	$scope.setDiaryFocus = function(diary){
+	$scope.setDiaryFocus = function(diary, pagenumber){
 		$scope.focusDiary = diary;
-		$http.get("data/diaryDetail/"+diary.nid+".json").success(function(data){
-			$scope.focusDiary.pages = data;
-		})
+
+		diary.expand = true;
+
+		if (diary.pages) {
+			$scope.setFocusPage(diary, pagenumber-1); // already loaded, just set the page
+		 } 
+		 else 
+		 {
+
+		//otherwise, load the data from file, then set the page
+
+			$http.get("data/diaryDetail/"+diary.nid+".json").success(function(data){
+				console.log("loaded diary " + diary.nid + " looking for page " + pagenumber);
+
+				diary.pages = data[0].pages; // store the page data on the diary data object
+				diary.page_no = data[0].page_no;
+				
+				$scope.setFocusPage(diary, pagenumber-1);
+
+				//console.log(data); // NB diary json is a one element array!!!!
+				
+				//diary.focusPage = data[0].pages[pagenumber-1];
+
+				//console.log($scope.focusDiary.nid);
+				//console.log($scope.focusPage.id);
+
+				
+			})
+		}
 	}
 
-	$scope.addOnClick = function(event, totalpages, diaryID) {
-		// Configuration paramenters for each layout (needs to be in  the top)
-		sq = 10; // n; n x n in pixels
-		ImgXLine = 120;
+
+	$scope.setFocusPage = function(diary, pageIndex){
+		console.log("setting page number " + pageIndex);
+
+		if (diary.pages.length <= pageIndex || pageIndex < 0) return;
+
+		diary.focusPageIndex = pageIndex;
+		
+		// set the focused page for this diary 
+		// to the value pageIndex - after checking the page 
+
+	}
+
+	$scope.addOnClick = function(event, totalpages, diary) {
+		// Configuration paramenters for each layout
+		sq = 10; // square dimansions: n -> n x n in pixels
+		ImgXLine = 120; // images (squares) per line
 
 		// Mouse coordinates
 		x = event.offsetX;
@@ -43,37 +82,33 @@ function listControl($scope,$http){
 			xmax = (totalpages%ImgXLine)*sq;
 			ymax = (Math.ceil(totalpages/ImgXLine) - 1)*sq;
 		}
+ 		my_page = (Math.ceil((y/sq)-1)*ImgXLine) + (Math.ceil((x/sq)%ImgXLine))
+ 		console.log(x, y, xmax, ymax, my_page, sq, ImgXLine, totalpages);
+ 		
+    x_norm = ((x/sq) - parseInt(x/sq))*sq;
+    y_norm = ((y/sq) - parseInt(y/sq))*sq;
+    if ((x_norm < 1 || x_norm  > 9) || ((y_norm < 1 || y_norm  > 9)) || (x > xmax && y > ymax )) {
+      output =   "("+parseInt(x/sq)+","+parseInt(y/sq)+") Not square clicked"
 
-   		my_page = (Math.ceil((y/sq)-1)*ImgXLine) + (Math.ceil((x/sq)%ImgXLine))
-   		//console.log(x, y, xmax, ymax, my_page, sq, ImgXLine, totalpages);
+    } else  {
+      output = "("+parseInt(x/sq)+","+parseInt(y/sq)+") Page clicked -> "+my_page+" | DiaryID: "+diary.nid;
+      // Show div on top
+      //$("#square").css("left",parseInt(3*((x/6)+1))).css("top",parseInt(3*((y/6)+1)));
 
-		return checkCoord(x, y, xmax, ymax, my_page, diaryID)
+    }
+    console.log(output);
+
+    $scope.setDiaryFocus(diary, my_page);
+
 	}
 
-    function checkCoord(x, y, xmax, ymax, my_page, diaryID) {
-		//console.log("x: "+x+" - xmax: "+xmax+" | y: "+y+" - ymax: "+ymax);
-        x_norm = ((x/sq) - parseInt(x/sq))*sq;
-        y_norm = ((y/sq) - parseInt(y/sq))*sq;
-        if ((x_norm < 1 || x_norm  > 9) || ((y_norm < 1 || y_norm  > 9)) || (x > xmax && y > ymax )) {
-          output =   "("+parseInt(x/sq)+","+parseInt(y/sq)+") Not square clicked"
 
-        } else  {
-          output = "("+parseInt(x/sq)+","+parseInt(y/sq)+") Page clicked -> "+my_page+" | DiaryID: "+diaryID;
-          // Show div on top
-          //$("#square").css("left",parseInt(3*((x/6)+1))).css("top",parseInt(3*((y/6)+1)));
-          // FIXME Mitchell? How we do this the best way? (ng-bind?)
-          window.location.href = '/pages.html?d=122345';
-        }
-        console.log(output);
-        return output;
-	}
 
 	$scope.pageClick = function(diaryID, pageID) {
 		// get text:
     $http.get("data/3-Transcriptions/"+diaryID+"/"+pageID+".json").success(function(data){
       mytext = data;
     })
-		// return html
     return mytext;
 
 	}
